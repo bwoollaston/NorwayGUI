290a491f1ba41d65963fb4ea6c48576b15cdd841
bwoollaston <cwoollas@gmail.com>
2023-02-17 19:34:11 -0500

    garbage

--- NorwayGUI/MainWindow.xaml.cs
+++ NorwayGUI/MainWindow.xaml.cs
@@ -21,6 +21,16 @@ namespace NorwayGUI
 {
     public partial class MainWindow : Window
     {
+        public bool LoadFlag
+        {
+            get { return loadFlag; }
+            set
+            {
+                loadFlag = value;
+                lblStatus();
+            }
+        }
+        public bool loadFlag = true;
         public Compiler compiler = new Compiler();
 
         public MainWindow()
@@ -34,15 +44,36 @@ namespace NorwayGUI
             {
                 compiler.Open();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                MessageBox.Show("CCS failed","Exception Sample",MessageBoxButton.OK,MessageBoxImage.Error);
+                MessageBox.Show("CCS failed", "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
         private void VoiceChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void lblStatus()
+        {
+            if (LoadFlag == true)
+            {
+                lblProgramSatus.Background = new SolidColorBrush(Colors.Red);
+                lblProgramSatus.Content = "Programing in progress";
+            }
+            else
+            {
+                lblProgramSatus.Background = new SolidColorBrush(Colors.Green);
+                lblProgramSatus.Content = "Ready to Program";
+            }
+        }
+
+
+        private void Test_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoadFlag == true) { loadFlag = false; }
+            else { loadFlag = true; }
         }
     }
 }
